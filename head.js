760a372fee2405ac825864c9a445a9ef320d9736
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-02-03 23:10:25 +0000

    Added head.js.

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃 Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`😭 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const head = function(arr) {
+  return arr[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([7, 8, 9]), 7);
+assertEqual(head([]), undefined);
