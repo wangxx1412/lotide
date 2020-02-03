479b9c7cb172a4410e0ee9a3c14fa471b3e62128
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-02-03 23:38:58 +0000

    Added tail.js

---
+++ tail.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃 Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`😭 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+//Test Case 1:
+const words = ["Hello", "Lighthouse", "Labs"];
+const result = tail(words);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+assertEqual(words.length, 3);
+
+//Test Case 2:
+const words2 = ["Hello"];
+const result2 = tail(words2);
+assertEqual(result2.length, 0);
+assertEqual(result2[0], undefined);
+assertEqual(words2.length, 1);
+
+//Test Case 3:
+const words3 = [];
+const result3 = tail(words3);
+assertEqual(result3.length, 0);
+assertEqual(result3[0], undefined);
+assertEqual(words3.length, 0);
