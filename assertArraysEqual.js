5727e5d12660c0b78c6b3fab6574fcc3fd837d70
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-02-04 22:15:29 +0000

    Complet assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(
+      "\x1b[32m%s\x1b[0m",
+      `\tPASS ✅ ${JSON.stringify(arr1)} is equal to ${JSON.stringify(arr2)}`
+    );
+    return;
+  }
+  console.log(
+    "\x1b[31m%s\x1b[0m",
+    `\tFAIL 🛑 ${JSON.stringify(arr1)} is NOT equal to ${JSON.stringify(arr2)}`
+  );
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2], [1, 2, 3]);
+assertArraysEqual([1, "3"], [1, "3"]);
+assertArraysEqual([0], ["0"]);
