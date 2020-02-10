29e55a5cc9c5e3f67051e7c699511604165ba505
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2020-02-10 09:55:10 -0800

    Add index.js contains all util functions

---
+++ index.js
@@ -0,0 +1,37 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const Pythagorean = require("./Pythagorean");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  Pythagorean,
+  takeUntil,
+  without
+};
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,6 @@
 const assert = require("chai").assert;
 const middle = require("../middle");
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// assertArraysEqual(middle([1]), []);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+
 describe("#middle", () => {
   it("returns [2] for [1,2,3]", () => {
     assert.deepEqual(middle([1, 2, 3]), [2]);
